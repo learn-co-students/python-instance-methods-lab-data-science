645734084987baea09f624be482c9c0ab5c11c22
tkoar <terrancekoar@gmail.com>
2018-09-18 10:36:27 -0400

    removed excess files

--- driver.py
+++
@@ -1,6 +0,0 @@
-# define Driver class and instance methods here
-
-# Note: remember that our instance methods need to be defined with the an
-# argument in order to be called on an instance of the class or else we will
-# get a `TypeError` telling us that we gave one too many arguments to the
-# function. We'll explain why that is soon.

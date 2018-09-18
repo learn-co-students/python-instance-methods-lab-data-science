bef6556e3a61340fd37fdc78a4045886b92b1dde
tkoar <terrancekoar@gmail.com>
2018-09-18 10:30:36 -0400

    renamed test dir -> pytests & test file -> test_index.py and added environment.yml

---
+++ pytests/test_index.py
@@ -0,0 +1,17 @@
+import pytest
+from ipynb.fs.full.index import *
+
+
+def test_instance_types():
+    assert type(meryl) == type(Driver())
+    assert type(daniel) == type(Driver())
+    assert type(niky) == type(Passenger())
+    assert type(terrance) == type(Passenger())
+
+def test_variables():
+    assert polite_greeting == "Hey, how are you?"
+    assert no_time_to_talk == "Punch it! They're on our tail!"
+
+def test_instance_methods():
+    assert daniel.greeting() == "Hey, how are you?"
+    assert niky.in_a_hurry() == "Punch it! They're on our tail!"

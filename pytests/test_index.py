a7343d78e07e7495fafec5019599938a94f2f110
tkoar <terrancekoar@gmail.com>
2018-09-18 10:31:11 -0400

    changing test to pytest

--- pytests/test_index.py
+++ pytests/test_index.py
@@ -1,18 +1,17 @@
-import unittest2 as unittest
+import pytest
 from ipynb.fs.full.index import *
 
-class TestInstanceMethods(unittest.TestCase):
 
-    def test_instance_types(self):
-        self.assertEqual(type(meryl), type(Driver()))
-        self.assertEqual(type(daniel), type(Driver()))
-        self.assertEqual(type(niky), type(Passenger()))
-        self.assertEqual(type(terrance), type(Passenger()))
+def test_instance_types():
+    assert type(meryl) == type(Driver())
+    assert type(daniel) == type(Driver())
+    assert type(niky) == type(Passenger())
+    assert type(terrance) == type(Passenger())
 
-    def test_variables(self):
-        self.assertEqual(polite_greeting, "Hey, how are you?")
-        self.assertEqual(no_time_to_talk, "Punch it! They're on our tail!")
+def test_variables():
+    assert polite_greeting == "Hey, how are you?"
+    assert no_time_to_talk == "Punch it! They're on our tail!"
 
-    def test_instance_methods(self):
-        self.assertEqual(daniel.greeting(), "Hey, how are you?")
-        self.assertEqual(niky.in_a_hurry(), "Punch it! They're on our tail!")
+def test_instance_methods():
+    assert daniel.greeting() == "Hey, how are you?"
+    assert niky.in_a_hurry() == "Punch it! They're on our tail!"
